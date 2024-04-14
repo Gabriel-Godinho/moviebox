381b1ddf94517ffd23a872d403733a298bc71126
Gabriel Godinho <gabriel234417@gmail.com>
2024-04-14 17:43:04 -0300

    DAO classes

---
+++ moviebox/src/dao/FilmeDAO.java
@@ -0,0 +1,21 @@
+package dao;
+
+import connection.DataBaseConnection;
+import model.Filme;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class FilmeDAO {
+
+    public final boolean save(Filme filme) {
+        try {
+            Connection conn = DataBaseConnection.getInstance().getConn();
+            String save = "";
+        } catch (SQLException e) {
+            System.out.println("Erro ao inserir o novo filme!");
+        }
+    }
+
+
+}
