7312bcc380d4452819d98d9b727646849c3f128c
jwrdwn <jordannogs@gmail.com>
2024-04-14 16:59:02 -0300

    Atualização main

--- moviebox/src/Main.java
+++ moviebox/src/Main.java
@@ -1,17 +1,26 @@
+import view.Entrar;
+
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("-----------------------------------");
-        System.out.println("|        Bem-vindo ao Sistema     |");
+        System.out.println("        Bem-vindo ao Sistema     ");
         System.out.println("-----------------------------------");
-        System.out.println("| Opções:                         |");
-        System.out.println("| 1. Entrar                       |");
-        System.out.println("| 2. Criar conta                  |");
-        System.out.println("| 3. Sair                         |");
+        System.out.println(" Opções:                         ");
+        System.out.println(" 1. Entrar                       ");
+        System.out.println(" 2. Criar conta                  ");
+        System.out.println(" 3. Sair                         ");
         System.out.println("-----------------------------------");
-        System.out.println("| Por favor, digite o número      |");
-        System.out.println("| correspondente à sua escolha:   |");
-        System.out.println("-----------------------------------");
+        System.out.println(" Por favor, digite o número      ");
+        System.out.print(" correspondente à sua escolha: ");
+
+        int escolha = scanner.nextInt();
+
     }
 }
