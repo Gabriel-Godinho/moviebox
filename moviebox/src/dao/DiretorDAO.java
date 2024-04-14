f25e7542b057a0e15619d9d9a0e7e261e5c7f8ab
Gabriel Godinho <gabriel234417@gmail.com>
2024-04-14 18:44:55 -0300

    DAOS

--- moviebox/src/dao/DiretorDAO.java
+++ moviebox/src/dao/DiretorDAO.java
@@ -1,4 +1,50 @@
 package dao;
 
+import connection.DataBaseConnection;
+import model.Diretor;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class DiretorDAO {
+
+    public void save(Diretor diretor) {
+        try {
+            Connection conn = DataBaseConnection.getInstance().getConn();
+            String sql = "INSERT INTO diretores(nome_diretor) VALUE(?)";
+
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1, diretor.getNomeDiretor());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Erro ao inserir o novo diretor!");
+        }
+    }
+
+    public void update(Diretor diretor) {
+        try {
+            Connection conn = DataBaseConnection.getInstance().getConn();
+            String sql = "UPDATE diretores SET nome_diretor = ?";
+
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1, diretor.getNomeDiretor());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Erro ao editar o diretor!");
+        }
+    }
+
+    public final void delete(long idDiretor) {
+        try {
+            Connection conn = DataBaseConnection.getInstance().getConn();
+            String sql = "DELETE FROM diretores WHERE id_diretor = ?";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setLong(1, idDiretor);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Erro ao excluir o diretor!");
+        }
+    }
+
 }
--- moviebox/src/dao/PaisOrigemDAO.java
+++ moviebox/src/dao/PaisOrigemDAO.java
@@ -1,4 +1,51 @@
 package dao;
 
+import connection.DataBaseConnection;
+import model.Diretor;
+import model.PaisOrigem;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class PaisOrigemDAO {
+
+    public void save(PaisOrigem paisOrigem) {
+        try {
+            Connection conn = DataBaseConnection.getInstance().getConn();
+            String sql = "INSERT INTO paises_origem(nome_pais) VALUE(?)";
+
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1, paisOrigem.getNomePais());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Erro ao inserir o novo país de origem!");
+        }
+    }
+
+    public void update(PaisOrigem paisOrigem) {
+        try {
+            Connection conn = DataBaseConnection.getInstance().getConn();
+            String sql = "UPDATE paises_origem SET nome_pais = ?";
+
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1, paisOrigem.getNomePais());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Erro ao editar o país de origem!");
+        }
+    }
+
+    public final void delete(long idPais) {
+        try {
+            Connection conn = DataBaseConnection.getInstance().getConn();
+            String sql = "DELETE FROM paises_origem WHERE id_pais = ?";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setLong(1, idPais);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Erro ao excluir país de origem!");
+        }
+    }
+
 }
