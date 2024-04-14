999d93c5f286444382b6afcba3aa10c7f8d7ded6
jwrdwn <jordannogs@gmail.com>
2024-04-14 18:02:04 -0300

    Filmes controller

--- moviebox/src/controllers/FilmesController.java
+++ moviebox/src/controllers/FilmesController.java
@@ -3,16 +3,24 @@ package controllers;
 import dao.FilmeDAO;
 import model.Filme;
 
+import java.sql.Connection;
+
 public class FilmesController {
 
     private final FilmeDAO filmeDAO = new FilmeDAO();
 
-    public void cadastrarFilme(String nomeFilme) {
-        Filme filme = new Filme();
+    public void cadastrarFilme(String nomeFilme, int duracao, int ano, long idDiretor, long idPais, String sinopse) {
+        Filme filme = new Filme(nomeFilme, duracao, ano, idDiretor, idPais, sinopse);
         filme.setNomeFilme(nomeFilme);
-        // Sets...
+        filme.setDuracao(duracao);
+        filme.setAno(ano);
+        filme.setIdDiretor(idDiretor);
+        filme.setIdPais(idPais);
+        filme.setSinopse(sinopse);
 
         filmeDAO.save(filme);
     }
 
+    public void cadastrarFilme(Connection connection) {
+    }
 }
