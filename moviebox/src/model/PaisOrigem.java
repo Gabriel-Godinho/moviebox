f30d5a3120ab7922ff27210b62a34b410f53e6be
jwrdwn <jordannogs@gmail.com>
2024-04-14 16:37:02 -0300

    Adicionando classe pais origem

---
+++ moviebox/src/model/PaisOrigem.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class PaisOrigem implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+}
