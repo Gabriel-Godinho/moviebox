7fdc5e5a69bbbf78573ab56fac70099fce0f687b
jwrdwn <jordannogs@gmail.com>
2024-04-14 16:45:08 -0300

    Adicionando tela de entrada

---
+++ moviebox/src/view/Entrar.java
@@ -0,0 +1,41 @@
+package view;
+
+import java.util.Scanner;
+
+public class Entrar {
+
+            Scanner scanner = new Scanner(System.in);
+
+            // Imprimir o layout de entrar
+            System.out.println("-----------------------------------");
+            System.out.println("|             Entrar              |");
+            System.out.println("-----------------------------------");
+            System.out.println("| Nome de usuário:                |");
+            System.out.println("| Senha:                          |");
+            System.out.println("-----------------------------------");
+            System.out.println("| Opções:                         |");
+            System.out.println("| 1. Entrar                       |");
+            System.out.println("| 2. Voltar                       |");
+            System.out.println("-----------------------------------");
+            System.out.println("| Digite o número correspondente  |");
+            System.out.println("| à sua escolha:                  |");
+            System.out.println("-----------------------------------");
+
+            // Ler a escolha do usuário
+            int escolha = scanner.nextInt();
+
+            // Processar a escolha do usuário
+            switch (escolha) {
+                case 1:
+                    // Lógica para entrar
+                    break;
+                case 2:
+                    // Lógica para voltar
+                    break;
+                default:
+                    System.out.println("Escolha inválida. Por favor, tente novamente.");
+            }
+
+            scanner.close();
+
+}
