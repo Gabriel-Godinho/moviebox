ba00f9cffce97e4700837575480936cc7e784d6c
jwrdwn <jordannogs@gmail.com>
2024-04-14 17:17:24 -0300

    Controllers

---
+++ moviebox/src/controllers/UsuariosController.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import model.Usuario;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class UsuariosController {
+    public void criarUsuario(Connection connection) throws SQLException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Para criar seu usuário, insira os dados:");
+        System.out.print("Nome de usuário: ");
+        String nomeUser = input.next();
+        System.out.print("Senha: ");
+        String senha = input.next();
+        System.out.print("Confirmar senha: ");
+        String confirmaSenha = input.next();
+        Usuario user = new Usuario(nomeUser, senha);
+        //Usuario.create(user, connection);
+        System.out.println("Usuário criado com sucesso!!");
+    }
+}
